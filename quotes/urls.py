e1721ae056e58b6f3d2d57099fdff5e97eb19643
utkarshg12anmak <utkarsh.gupta@anmaksolar.com>
2025-07-05 00:23:38 +0530

    v1.2.81

--- quotes/urls.py
+++ quotes/urls.py
@@ -23,10 +23,6 @@ urlpatterns = [
     path("approvals/", views.QuoteApprovalListView.as_view(), name="approval_list"),
     path("approvals/<int:pk>/approve/",  views.approve_quote,  name="approve_quote"),
     path("approvals/<int:pk>/decline/",  views.decline_quote,  name="decline_quote"),
-    path('<int:quote_pk>/download-department-draft/',
-      views.download_department_draft_pdf,
-      name='download_department_draft_pdf'
-    ),    
     path("quotation/<int:pk>/", quotation_view, name="quotation_view"),    
     path(
         "preview-quote/<int:quote_pk>/",
